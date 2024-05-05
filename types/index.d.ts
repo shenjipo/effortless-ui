3bfea4f22d2ac166fbdab3a8efe77f18f7f89524
wang <1532281546@qq.com>
2024-05-05 21:56:13 +0800

    添加类型

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,4 +1,15 @@
-export * from './element-ui'
 
-import * as ElementUI from './element-ui'
-export default ElementUI
+export * from './effortless-ui'
+
+import * as EffortlessUI from './effortless-ui'
+
+import Vue, { PluginObject } from 'vue'
+export namespace CommonUtils {
+    function JsDialog(vue: typeof Vue, props: Record<string, any>): {
+        show: () => {}
+    }
+
+}
+
+
+export default EffortlessUI
